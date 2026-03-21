8e001a592f6f49e67aff79c776620a16058c4ef0
varshithRaj02 <varshithvadla@gmail.com>
2026-03-21 12:58:01 +0530

    added create_db.py for postgres

--- create_db.py
+++ create_db.py
@@ -1,9 +1,8 @@
 from app.app import create_app
 from app.extensions import db
-import app.models.models  # ✅ THIS is the correct import
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
-    print("Database created successfully")
+    print("✅ Tables created successfully")
