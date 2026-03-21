98cc8dc0e42c4cc4f6b49e45c0ed1a0882aa613f
varshithRaj02 <varshithvadla@gmail.com>
2026-03-21 12:22:20 +0530

    fixed run.py and postgres config

--- app/config.py
+++ app/config.py
@@ -9,7 +9,12 @@ MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
 class Config:
     SECRET_KEY = "super-secret-key"
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "civic.db")
+    db_url = os.getenv("DATABASE_URL") or "sqlite:///civic.db"
+
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://")
+
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "app/static/uploads"
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
