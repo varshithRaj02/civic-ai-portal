b4dfc97108030036f9933700cc32db408be6879a
varshithRaj02 <varshithvadla@gmail.com>
2026-03-21 12:37:30 +0530

    fixed run.py and postgres config

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
 from app.app import create_app
-from app.extensions import db
-
-
-
-
 app = create_app()
-with app.app_context():
-    db.create_all()
-print(app.url_map)
 if __name__ == "__main__":
     app.run(debug=True)
